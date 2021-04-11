e8d308cbde6d48fe94781ab8070cfa2f2834f85f
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-04-11 12:56:47 +0800

    Update website

--- Gemfile
+++ Gemfile
@@ -5,5 +5,3 @@ gem "jekyll-octicons"
 gem "jemoji"
 
 gem "jekyll-assets", group: :jekyll_plugins
-gem "jekyll-assets", git: "https://github.com/envygeeks/jekyll-assets", group: :jekyll_plugins
-gem "jekyll-assets", "~> x.x.alpha", group: :jekyll_plugins
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    // webpack folder’s entry js — excluded from jekyll’s build process.
+    entry: "./webpack/entry.js",
+    output: {
+        // we’re going to put the generated file in the assets folder so jekyll will grab it.
+        // if using GitHub Pages, use the following:
+        // path: "assets/js"
+        path: "src/assets/js/",
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                loader: "babel", // "babel-loader" is also a legal name to reference
+                query: {
+                    presets: ["react", "es2015"]
+                }
+            }
+        ]
+    }
+};
